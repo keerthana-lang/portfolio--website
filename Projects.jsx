27896124d08999d2531219a529c292ea9df86ca5
Keerthana/345 <nanasoundari410@gmail.com>
2025-08-11 19:16:26 +0530

    Update Projects.jsx

--- Projects.jsx
+++ Projects.jsx
@@ -12,6 +12,11 @@ function Projects() {
   description: "A full-featured task manager with create, edit, and delete functionality. Built using React, Tailwind CSS, and local storage for data persistence.",
   link: "https://github.com/keerthana-lang/TASK_MANAGER", // 🔁 Replace with your real live link
 },
+   {
+  title: "Translator Random",
+  description: "A fun project that combines a translator with a random generator offering quotes, jokes, and words for daily inspiration and entertainment.",
+  link: "https://github.com/keerthana-lang/translator", // 🔁 Replace with your real live link
+},
     {
       title: "Portfolio Website",
       description: "Personal portfolio built with React and Vite.",
@@ -20,7 +25,7 @@ function Projects() {
   ];
 
   return (
-    <section className="py-16 px-7 bg-gray-200">
+    <section className="py-16 px-7 bg-gradient-to-r from-indigo-200 via-blue-400 to-purple-400 min-h-screen">
       <h2 className="text-3xl font-bold text-center text-indigo-600 mb-10">My Projects</h2>
       <div className="grid md:grid-cols-3 gap-8">
         {projectList.map((project, index) => (
